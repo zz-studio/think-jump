fc1340d67f6b019dc6ffd96eeb594596b7d6f5d1
小波🌾 <xiaobosun@gridinfo.com.cn>
2019-11-04 15:01:04 +0800

    update

---
+++ src/config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ * | aoxuan [PhpStorm]
+ * +----------------------------------------------------------------------
+ *  .--,       .--,             | FILE: config.php
+ * ( (  \.---./  ) )            | AUTHOR: byron
+ *  '.__/o   o\__.'             | EMAIL: xiaobo.sun@qq.com
+ *     {=  ^  =}                | QQ: 150093589
+ *     /       \                | DATETIME: 2019/11/4 14:52
+ *    //       \\               |
+ *   //|   .   |\\              |
+ *   "'\       /'"_.-~^`'-.     |
+ *      \  _  /--'         `    |
+ *    ___)( )(___               |-----------------------------------------
+ *   (((__) (__)))              | 高山仰止,景行行止.虽不能至,心向往之。
+ * +----------------------------------------------------------------------
+ * | Copyright (c) 2019 http://www.zzstudio.net All rights reserved.
+ * +----------------------------------------------------------------------
+ */
+return [
+    // 默认跳转页面对应的模板文件
+    'dispatch_success_tpl' => app()->getRootPath().'/vendor/zzstudio/think-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_error_tpl'   => app()->getRootPath().'/vendor/zzstudio/think-jump/src/tpl/dispatch_jump.tpl',
+];
